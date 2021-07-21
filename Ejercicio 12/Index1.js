13e0f55783d7ee9cab406e3f49e53cc075a8e038
cesarguerramendez <84873910+cesarguerramendez@users.noreply.github.com>
2021-07-21 08:57:48 +0200

    Add files via upload

---
+++ Ejercicio 12/Index1.js
@@ -0,0 +1,6 @@
+'use strict';
+
+fetch('https://rickandmortyapi.com/api/character/1')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
